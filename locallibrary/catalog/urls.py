a724c380564804c70e55bc11bdb2e4ebec0dce34
Shaaz Anaam <shaazanaam@gmail.com>
2024-11-28 17:57:52 -0600

    Create Skeleton framework for Local Library

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,9 @@
+# Description: URL Configuration for the catalog application
+#the text defines the imported urlpatterns
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+]
